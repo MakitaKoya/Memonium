af9c8e32a880735a1b496323bc1896eee294eefa
MakitaKoya <g2481454@tcu.ac.jp>
2025-01-19 21:25:52 +0900

    Fix: Fixed an issue that caused the UI to be displayed incorrectly.

--- src/Pages/UrlCollectionPage.tsx
+++ src/Pages/UrlCollectionPage.tsx
@@ -121,6 +121,7 @@ export const UrlCollectionPage: React.FC = () => {
             width="100%"
             height="100%"
             margin="10px 0px"
+            paddingBlockEnd="100px"
             overflowY="scroll"
           >
             {isDataLoaded ? (
